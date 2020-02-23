f0049d1dabdc0a93b691694e4fa0d9e986b44864
gordonzu <gordonzu@outlook.com>
2020-02-22 20:47:00 -0500

    finish xy_environment agent tests

--- tests/unit_tests/xyenvironment_test.cpp
+++ tests/unit_tests/xyenvironment_test.cpp
@@ -34,3 +34,144 @@ TEST_F(TestXYEnvironment, testAddObject2) {
     EXPECT_EQ(env.get_num_objects(), size_t(2));
     EXPECT_EQ(env.get_num_agents(), size_t(1));
 }
+
+TEST_F(TestXYEnvironment, test_add_object_twice) {
+    EXPECT_EQ(env.objects_at_location(loc), size_t(1));
+    XYLocation xy{5,5};
+    env.add(agent, xy);
+    EXPECT_EQ(env.objects_at_location(xy), size_t(1));
+    EXPECT_EQ(env.get_location(agent), xy);
+}
+
+TEST_F(TestXYEnvironment, test_objects_are_unique) {
+    EnvPtr agent2 = std::make_shared<MockAgent>();
+    EXPECT_NE(agent, agent2);
+}
+
+TEST_F(TestXYEnvironment, test_add_two_objects_one_location) {
+    XYLocation xy{9,8};
+    env.move_to_absolute_location(agent,xy);
+    EXPECT_EQ(env.objects_at_location(xy), size_t(1));
+    EnvPtr agent2 = std::make_shared<MockAgent>();
+    env.move_to_absolute_location(agent2,xy);
+    EXPECT_EQ(env.objects_at_location(xy), size_t(2));
+}
+
+TEST_F(TestXYEnvironment, change_object_location) {
+    XYLocation xy{5,6};
+    env.add(agent, xy);
+    EXPECT_EQ(env.objects_at_location(xy), size_t(1));
+    EXPECT_EQ(env.objects_at_location(loc), size_t(0));
+}
+
+TEST_F(TestXYEnvironment, test_get_current_location) {
+    XYLocation xy{3,4};
+    EXPECT_EQ(env.get_location(agent), xy);
+    XYLocation xy2{12,5};
+    EXPECT_NE(env.get_location(agent), xy2);
+}
+
+TEST_F(TestXYEnvironment, test_move_to_absolute_location) {
+    XYLocation xy{5,3};
+    env.move_to_absolute_location(agent, xy);
+    EXPECT_EQ(env.get_location(agent), xy);
+    EXPECT_NE(env.get_location(agent), loc); 
+}
+
+TEST_F(TestXYEnvironment, test_move_object) {
+    XYLocation xy{5,5};
+    env.add(agent, xy);
+    EXPECT_EQ(env.get_location(agent), xy);
+
+    XYLocation loc54{5,4};
+    XYLocation loc64{6,4};
+    XYLocation loc65{6,5};
+
+    env.move_object(agent, XYLocation::Direction::NORTH);
+    EXPECT_EQ(env.get_location(agent), loc54);
+    env.move_object(agent, XYLocation::Direction::EAST);
+    EXPECT_EQ(env.get_location(agent), loc64);
+    env.move_object(agent, XYLocation::Direction::SOUTH);
+    EXPECT_EQ(env.get_location(agent), loc65);
+    env.move_object(agent, XYLocation::Direction::WEST);
+    EXPECT_EQ(env.get_location(agent), xy);
+}
+
+TEST_F(TestXYEnvironment, test_is_blocked) {
+    XYLocation xy{5,5};
+    env.add(agent, xy);
+    EXPECT_EQ(env.blocked(xy), false);
+    env.add(wall, xy);
+    EXPECT_EQ(env.blocked(xy), true);
+}
+
+TEST_F(TestXYEnvironment, test_move_with_blocking_walls) {
+    XYLocation xy{5,5};
+    env.add(agent, xy);
+
+    XYLocation xyNorth{5,6};
+    XYLocation xySouth{5,4};
+    XYLocation xyWest{4,5};
+    XYLocation xyEnd{6,5};
+
+    EnvPtr northwall = std::make_shared<Wall>();
+    EnvPtr southwall = std::make_shared<Wall>();
+    EnvPtr westwall = std::make_shared<Wall>();
+    env.add(northwall, xyNorth);
+    EXPECT_EQ(env.blocked(xyNorth), true);
+   
+    env.add(southwall, xySouth);
+    env.add(westwall, xyWest);
+    EXPECT_EQ(env.get_num_objects(), size_t(4));
+    
+    env.move_object(agent, XYLocation::Direction::NORTH); // should not move
+    env.move_object(agent, XYLocation::Direction::SOUTH); // should not move
+    env.move_object(agent, XYLocation::Direction::WEST);  // should not move
+    env.move_object(agent, XYLocation::Direction::EAST);  // SHOULD move
+    EXPECT_EQ(env.get_location(agent), xyEnd);
+}
+
+TEST_F(TestXYEnvironment, test_get_objects_near) {
+    XYLocation loc{5,5};
+    XYLocation loc2{7,4};
+    XYLocation loc3{5,7};
+    XYLocation loc4{3,10};
+    XYLocation loc5{3,11};
+    
+    EnvPtr b = std::make_shared<MockAgent>();
+    EnvPtr c = std::make_shared<MockAgent>();
+    EnvPtr w = std::make_shared<MockAgent>();
+
+    env.add(agent, loc);
+    env.add(b, loc2);
+    env.add(c, loc3);
+    env.add(w, loc4);
+
+    EXPECT_EQ(env.get_objects_near(agent, 3).size(), size_t(2));
+    EXPECT_EQ(env.get_objects_near(b, 3).size(), size_t(1));
+
+    env.move_object(b, XYLocation::Direction::SOUTH);
+    EXPECT_EQ(env.get_objects_near(b, 3).size(), size_t(2));
+
+    env.add(c, loc5);
+    EXPECT_EQ(env.get_objects_near(c, 4).size(), size_t(1));
+}
+
+TEST_F(TestXYEnvironment, test_make_perimeter) {
+    env.make_perimeter(5);
+
+    EXPECT_TRUE(env.blocked(XYLocation{1,1}));
+    EXPECT_TRUE(env.blocked(XYLocation{1,2}));
+    EXPECT_TRUE(env.blocked(XYLocation{1,5}));
+    EXPECT_TRUE(env.blocked(XYLocation{2,1}));
+    EXPECT_TRUE(env.blocked(XYLocation{5,2}));
+    EXPECT_TRUE(env.blocked(XYLocation{5,5}));
+    EXPECT_TRUE(env.blocked(XYLocation{4,1}));
+
+    EXPECT_FALSE(env.blocked(XYLocation{7,5}));
+    EXPECT_FALSE(env.blocked(XYLocation{2,8}));
+    EXPECT_FALSE(env.blocked(XYLocation{4,4}));
+    EXPECT_FALSE(env.blocked(XYLocation{3,2}));
+}
+
+
