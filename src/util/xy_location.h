577ee3c3169d7204f58209db2af05c536055e5e1
gordonzu <gordonzu@outlook.com>
2020-02-22 19:38:47 -0500

    test addObject2

--- src/util/xy_location.h
+++ src/util/xy_location.h
@@ -11,7 +11,132 @@
 
 class XYLocation {
 public:
-    XYLocation();
+    enum class Direction: unsigned short{ NORTH, SOUTH, EAST, WEST };
+
+    XYLocation() {}
+    XYLocation(int x, int y): x_{x}, y_{y} {}
+    ~XYLocation() {}
+
+    XYLocation(const XYLocation& other) {
+        x_ = other.x_;
+        y_ = other.y_;
+    }
+
+    XYLocation(XYLocation& other) {
+        x_ = other.x_;
+        y_ = other.y_;
+    }
+
+    void print() {
+        std::cout << "{" << x_ << ", " << y_ << "}" <<  std::endl;
+    }
+
+    int getx() const {
+        return x_;
+    }
+
+    int gety() const {
+        return y_;
+    }
+
+    XYLocation& operator=(const XYLocation& rhs) {
+        if (&rhs != this) {
+            x_ = rhs.x_;
+            y_ = rhs.y_;
+        }
+        return *this;
+    }
+
+    bool operator<(const XYLocation& rhs) const {
+        if ((x_ < rhs.x_) || (y_ < rhs.y_))     
+            return true;
+        return false;
+    } 
+
+    bool operator==(const XYLocation& rhs) const  {
+            return ((x_ == rhs.x_) && (y_ == rhs.y_));
+    }
+
+    bool operator!=(const XYLocation& rhs) const  {
+            return ((x_ != rhs.x_) || (y_ != rhs.y_));
+    }
+
+    XYLocation west() {
+        XYLocation tmpxy{x_ - 1, y_};
+        return tmpxy;
+    }
+
+    XYLocation east() {
+        XYLocation tmpxy{x_ + 1, y_};
+        return tmpxy;
+    }
+
+    XYLocation north() {
+        XYLocation tmpxy{x_, y_ -1};
+        return tmpxy;
+    }
+
+    XYLocation south() {
+        XYLocation tmpxy{x_, y_ + 1};
+        return tmpxy;
+    }
+
+    XYLocation right() {
+        return east();
+    }
+
+    XYLocation left() {
+        return west();
+    }
+
+    XYLocation up() {
+        return north();
+    }
+
+    XYLocation down() {
+        return south();
+    }
+
+    XYLocation location_at(const Direction& direction) {
+        XYLocation xy;
+        try {
+            switch (direction) {
+                case     Direction::NORTH:
+                         xy = north();
+                         break;
+                case     Direction::SOUTH:
+                         xy = south();
+                         break;
+                case     Direction::EAST:
+                         xy = east();
+                         break;
+                case     Direction::WEST:
+                         xy = west();
+                         break;
+                default: throw std::runtime_error("Unknown direction...");
+            } 
+        }
+        catch(std::exception const& e) {
+            std::cerr << "Exception: " << e.what() << "\n";
+        }
+        return xy;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const XYLocation& xy) {
+        out << "{" << xy.x_ << ", " << xy.y_ << "}";
+        return out;
+    }
+
+private:
+    int x_;
+    int y_;
+};
+#endif
+
+
+
+
+/*        XYLocation();
     XYLocation(int x, int y);
     ~XYLocation();
     XYLocation(const XYLocation& other);
@@ -42,6 +167,4 @@ public:
 private:
     int x_;
     int y_;
-};
-#endif
-
+*/
--- tests/unit_tests/xyenvironment_test.cpp
+++ tests/unit_tests/xyenvironment_test.cpp
@@ -11,7 +11,6 @@ using namespace::testing;
 class TestXYEnvironment: public Test {
 public:
     TestXYEnvironment() {
-        //agent = std::make_unique<MockAgent>();
         agent = std::make_shared<MockAgent>();
         wall = std::make_shared<Wall>();
         env.add(agent, loc);        
@@ -26,4 +25,12 @@ public:
 TEST_F(TestXYEnvironment, testAddAgent) {
     EXPECT_EQ(env.get_location(agent), loc);
     EXPECT_EQ(env.objects_at_location(loc), size_t(1));
+}
+
+TEST_F(TestXYEnvironment, testAddObject2) {
+    XYLocation yy{9,9};
+    env.add(wall, yy);
+    EXPECT_EQ(env.objects_at_location(yy), size_t(1));
+    EXPECT_EQ(env.get_num_objects(), size_t(2));
+    EXPECT_EQ(env.get_num_agents(), size_t(1));
 }
