9a612258891813197955c9ff14ee50ea5e95144b
gordonzu <gordonzu@outlook.com>
2020-02-26 21:06:19 -0500

    finish test attribute map

--- src/util/utilities.h
+++ src/util/utilities.h
@@ -16,7 +16,20 @@ using attribute_map = std::map<Attribute, Attribute>;
 using attribute_table = std::map<Attribute, attribute_map>; 
 
 namespace ut {
+    inline std::string map_to_string(const attribute_map& x) {
+            std::stringstream out;	
+            out << "<map>" << std::endl;
 
+            for (const auto& e: x) {
+                out << std::string(2, ' ') 
+                    << "key: " << print_string(e.first) << ", " 
+                    << "value: " << print_string(e.second)  
+                    << std::endl;
+            }
+            out << "</map>" << std::endl;
+            std::string str_ = out.str();
+            return str_; 
+        }
 
 
 }
--- tests/unit_tests/utilities_test.cpp
+++ tests/unit_tests/utilities_test.cpp
@@ -27,10 +27,82 @@ public:
     attribute_map attribMap;
     attribute_map::iterator it;   
     std::stringstream str_out;
+
+        
 };
 
-TEST_F(TestUtilities, test_attribute_map) {
+TEST_F(TestUtilities, test_size) {
     EXPECT_TRUE(attribMap.size() == 7);
-
 }
  
+TEST_F(TestUtilities, test_integerkey_lookup) {
+    it = attribMap.find(0);
+    str_out << it->first << ", " << it->second;
+    EXPECT_STREQ(str_out.str().c_str(), "0, 2");
+}
+
+TEST_F(TestUtilities, test_stringLiteral_key_lookup) {
+    it = attribMap.find("key1");
+    str_out << it->first << ", " << it->second;
+    EXPECT_STREQ(str_out.str().c_str(), "key1, val1");
+}
+
+TEST_F(TestUtilities, test_retrieve_boolean_attribute) {
+    it = attribMap.find("key2");
+    str_out << it->first << ", " << it->second;
+    EXPECT_STREQ(str_out.str().c_str(), "key2, true");
+}
+
+TEST_F(TestUtilities, test_stringVariable_key_lookup) {
+    it = attribMap.find(key1);
+    str_out << it->first << ", " << it->second;
+    EXPECT_STREQ(str_out.str().c_str(), "key55, value55");
+}
+
+TEST_F(TestUtilities, test_map_string_representation) {
+    std::string strMap = "<map>\n";
+    strMap += "  key: 0, value: 2\n"; 
+    strMap += "  key: key1, value: val1\n"; 
+    strMap += "  key: key2, value: true\n"; 
+    strMap += "  key: key55, value: value55\n"; 
+    strMap += "  key: key66, value: value66\n"; 
+    strMap += "  key: key77, value: 547996\n"; 
+    strMap += "  key: key88, value: false\n"; 
+    strMap += "</map>\n";
+
+    EXPECT_TRUE(attribMap.size() == 7);
+    auto str = ut::map_to_string(attribMap);
+    EXPECT_STREQ(strMap.c_str(), str.c_str());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
